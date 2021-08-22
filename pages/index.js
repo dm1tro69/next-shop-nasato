62c5e08937fc448119464c0b4eb22b9d2e7139f5
dm1tro69 <dimolg22@gmail.com>
2021-08-22 22:22:51 +0300

    add css

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from 'next/image'
 import Title from "../components/Title";
 import {getProducts} from "../lib/products";
+import ProductCard from "../components/ProductCard";
 
 
 export default function HomePage({products}) {
@@ -13,11 +14,11 @@ export default function HomePage({products}) {
       </Head>
       <main className={'px-6 py-4'}>
        <Title>Next Shop</Title>
-          <ul>
+          <ul className={'grid grid-cols-1 lg:grid-cols-3 gap-4'}>
               {products.map((product) => {
                   return (
                       <li key={product.id}>
-                          <Link href={`/products/${product.id}`}><a>{product.title}</a></Link>
+                          <ProductCard product={product}/>
                       </li>
                   )
               })}
--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Title from "../../components/Title";
 import Head from "next/head";
+import Image from "next/image";
 import {getProduct, getProducts} from "../../lib/products";
 import {ApiError} from "../../lib/api";
 
@@ -16,7 +17,16 @@ const ProductPage = ({product}) => {
         <main>
             <Title>Product Page</Title>
             <h1>{product.title}</h1>
-            <p>{product.description}</p>
+            <div className={'flex flex-col lg:flex-row'}>
+                <div className={''}>
+                    <Image src={product.pictureUrl} height={480} width={640}/>
+                </div>
+
+                <div className={'flex-1 lg:ml-4'}>
+                    <p>{product.description}</p>
+                    <p className={'text-lg font-bold mt-2'}>{product.price}</p>
+                </div>
+            </div>
         </main>
         </>
     );
